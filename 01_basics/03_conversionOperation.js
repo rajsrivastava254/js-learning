bcf53d16ef8886bf2e8b4e4621e4bc6d8cd09052
Raj Srivastava <92249213+rajsrivastava254@users.noreply.github.com>
2024-12-02 12:59:51 +0000

    conversion and operation dded

---
+++ 01_basics/03_conversionOperation.js
@@ -0,0 +1,30 @@
+let score = "33"
+console.log(typeof score);
+
+let valueInNumeber = Number(score)
+console.log(typeof valueInNumeber)
+console.log(valueInNumeber);
+// if number is in string then it will convert
+// if the value is alphanumeric then it print NaN
+//if the score is true it will print 1 an for false it print 0
+// for null value it will print 0
+//for undefied it will print NaN
+// ""-> false
+//"hello" -> true
+
+let sum = 33
+let stringNumber = String(sum)
+console.log(stringNumber);
+
+console.log(typeof stringNumber);
+
+//**************Operations************ */
+// i am not covering basic sum subtract operation it is same as we learn before
+
+console.log("1"+2);
+console.log(1+"2");
+console.log("1"+2+2); //if first value is string it treat all as string
+console.log(1+2+"2"); //if first is numeric then it sum then add string. we not use this in real life
+//learn prefix and postfix incremental
+
+
