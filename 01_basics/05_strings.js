33648043a9e5dd35d76360402e16fdaf6d944297
Raj Srivastava <92249213+rajsrivastava254@users.noreply.github.com>
2024-12-04 13:15:26 +0000

    string learning

---
+++ 01_basics/05_strings.js
@@ -0,0 +1,18 @@
+let name  = "Raj"
+let followers = 1000
+
+//use String Interpolation by using `(this present in above Tab button)
+console.log(`My name is ${name} and i have ${followers} followers`);
+
+/*
+Some important methods are:
+1.toLowerCase()
+2.tocharAt(i) i is integer
+3.indexOf('t') any char index
+4.substring(0,4) it returns 0 to 3 index value, it only start with zero or +ve value
+5.slice(-8,5) it also take -ve value
+6.trim() it remove excess or extra spaces of left and right side of string
+7.replace(hi, hello) it replace two value in string
+8.includes() it returns boolean
+9.spilt(" ") this splits the string value on the basis of space and convert into arrayList
+*/
