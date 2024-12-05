188384b8e26033bae77b503d9dff7d6aec633a9b
Raj Srivastava <92249213+rajsrivastava254@users.noreply.github.com>
2024-12-05 13:02:09 +0000

    Math and num

---
+++ 01_basics/06_mathAndNumber.js
@@ -0,0 +1,28 @@
+const num = 10
+const num1 = new Number(100)
+// console.log(num1);
+// console.log(num1.toString()); //converted to string
+// console.log(num1.toFixed(2)); //it gives two extra decimal value
+
+const num2 = 123.8999;
+// console.log(num2.toPrecision(4)); //it precise the value to 4 digits
+
+//++++++++++++++++++++++++++++++++++MATHS+++++++++++++++++++++++++++++
+
+// console.log(Math.abs(-4)); //it convert -ve to +ve
+// console.log(Math.round(4.76)); // it round off
+//there are many methods under Math library such as max, min, floor, ceil,random
+//  console.log(Math.random());  //it gives random value between 0 to 1
+//  console.log(Math.floor(Math.random() *10) +1);
+
+ const min = 10
+ const max = 20
+ console.log(Math.floor(Math.random() *(max-min+1)) +min); //this print random value between 10 to 20
+ 
+
+
+
+
+
+
+
