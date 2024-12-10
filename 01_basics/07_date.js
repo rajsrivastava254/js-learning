35e546f75dd584e49d75477c29ab6d1ae0fb22d9
Raj Srivastava <92249213+rajsrivastava254@users.noreply.github.com>
2024-12-10 12:40:16 +0000

    date added

---
+++ 01_basics/07_date.js
@@ -0,0 +1,21 @@
+let myDate = new Date
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(typeof myDate);
+
+let myCreatedDate = new Date("1-12-2023")
+// console.log(myCreatedDate.toLocaleString());
+
+let myTimeStamp = Date.now();
+console.log(myTimeStamp); //it shows in milisecond
+// console.log(Math.floor(myTimeStamp/1000));
+
+
+
+
+
+
+
+
