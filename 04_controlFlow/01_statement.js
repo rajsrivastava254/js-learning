7ad0802c1c97946f3744714caa22ad5ec45a6e8c
Raj Srivastava <92249213+rajsrivastava254@users.noreply.github.com>
2024-12-16 06:30:51 +0000

    control flow added

---
+++ 04_controlFlow/01_statement.js
@@ -0,0 +1,21 @@
+const isUserLoggedIn = true
+if(isUserLoggedIn){
+    // console.log("executed");
+    
+}
+//we use differnt comparison operator in if condition
+//like=> <,>,==,===(it check both value and datatype),! etc.
+const balance = 1000
+// if (balance>500) console.log("ok"); //implicit if condition
+
+if (balance<500) {
+    console.log("less than 500"); 
+}
+else if (balance<750) {
+    console.log("less than 1000");
+    
+} else {
+    console.log("equal to 1000"); 
+}
+//similar with logical operator such as &&, ||,!
+
---
+++ 04_controlFlow/02_switch.js
@@ -0,0 +1,28 @@
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// } basic syntax
+
+const month = 3
+switch (month) {
+    case 1:
+        console.log("January");
+        break;
+    case 2:
+        console.log("february");
+        break;
+    case 3:
+        console.log("march");
+        break;
+    case 4:
+        console.log("April");
+        break;
+
+    default:
+        console.log("Enter correct value between 1 to 12");
+        break;
+}
